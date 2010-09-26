a02482c6c72a774904b8fc145a33563c78ec897b
Jack Wakefield <jackwakefield@protonmail.com>
2010-09-26 20:33:35 +0000

    - Missing files

---
+++ SharpAPI.Interface/Internal/ROSE/Interface/IT_MGR.cs
@@ -0,0 +1,28 @@
+﻿/*
+ *  This file is a part of SharpAPI.
+ *
+ *  Copyright (C) 2010 Jack Wakefield
+ *  
+ *  SharpAPI is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  SharpAPI is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with SharpAPI.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+
+namespace SharpAPI.Internal.ROSE.Interface {
+    internal static class IT_MGR {
+        internal delegate void AppendDlgDelegate(short type, IntPtr dialog, int id = 0);
+
+        internal static AppendDlgDelegate AppendDlg;
+    }
+}
